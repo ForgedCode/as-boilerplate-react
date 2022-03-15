e6b9dfefcc28e33a2a873ac93f056fbc35751aef
Amirkhon <amirkhon.shakirov@gmail.com>
2022-03-15 16:34:12 +0500

    boilerplate-v1

---
+++ src/styles/variables.js
@@ -0,0 +1,19 @@
+export const v = {
+	headerHeight: "5rem",
+	smallDist: "1rem",
+	mediumDist: "2rem",
+	largeDist: "3rem",
+	xLargeDist: "4rem",
+	xxLargeDist: "5rem",
+};
+
+export const b = {
+	medium: `(min-width: 600px)`,
+	large: `(min-width: 1200px)`,
+};
+
+export const t = {
+	fastSp: `all 0.3s ease`,
+	midSp: `all 0.5s ease`,
+	lowSp: `all 1s ease`,
+};
